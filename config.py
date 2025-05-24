fd53dfad2d293228c99a4e3ed05778e1a183682a
theb7r <brb99054@gmail.com>
2025-05-24 07:08:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-API_ID = 123456  # استبدل هذا بـ api_id من my.telegram.org
-API_HASH = "your_api_hash"  # استبدل هذا بـ api_hash من my.telegram.org
-BOT_TOKEN = "توكن البوت من BotFather"
+API_ID = 23090097  # استبدل هذا بـ api_id من my.telegram.org
+API_HASH = "3fb3746ba526a5b95fc8205d7015c0e5"  # استبدل هذا بـ api_hash من my.telegram.org
+BOT_TOKEN = "7670972568:AAHQwiHw7W9FyzXznqAks8RXcfT4HXpby_k"
