3da7f7cf7e5fc4f4a2823c455e7db7197224f44c
theb7r <brb99054@gmail.com>
2025-07-09 22:13:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 API_ID = 23090097  # استبدل هذا بـ api_id من my.telegram.org
 API_HASH = "3fb3746ba526a5b95fc8205d7015c0e5"  # استبدل هذا بـ api_hash من my.telegram.org
-BOT_TOKEN = "7670972568:AAHQwiHw7W9FyzXznqAks8RXcfT4HXpby_k"
+BOT_TOKEN = "7867489956:AAFDzQVIuAXLHtwzJno4hrLy9pXEWA22GvA_k"
