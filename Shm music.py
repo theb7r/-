ad04b549385e854d736d26049195aa651ed84b71
theb7r <brb99054@gmail.com>
2025-07-15 13:36:45 +0200

    Update  Shm music.py

---
+++ Shm music.py
@@ -0,0 +1,16 @@
+##مطور السورس
+caes = ["div_victor"]
+casery = "div_victor"
+##ايدي مطور السورس
+caserid = 7934749229
+##قناه السورس
+source = "https://t.me/CORANBM" 
+ch = "JO7NB" 
+##جروب السورس
+group = "https://t.me/CV_SS" 
+##لوجو السورس
+photosource = "https://t.me//"
+##توكن الصانع
+bot_token="7910609107:AAEamniPWG2J5f8sQkKnUjv0nVu_yCGUtBE"
+##جلسه الاشتراك
+bot_token2="BAFcX10ALH7AEeOGHzSqNqO9kPTu44jp2OadEN6I6b5MG7yA2PLqg3zy8eOjG3jxp9fbm__GWOAhGs9ga16yFc3Fjo7isbAH3cZwO7GUOaVMZ1LvEVN8ay7yLL7sRo9O70KuSYxslCcjPjIDvnwbyNRNFR9paYfLabdIE5oOAP1Ue1Fi9dtSiEalnpvRFHLyri-P-Q0tdi_wZ5V9JBeiGEsvFi9NJATh61Xl1ST3TG6lmRBSJhTGwLnUfDgxZ98PXbqK3cD3oLl3lhQCUdbC90Pf_gGvdtIWTobUpaAaxDyu7UmPdTscD-Y18wN0X-TkxrTKXJDLAlBpIQx5tXl0q-T82i5fbQAAAAGwxrx3AA"
